eb3919a56428af04ccdd71948b9b782194f0507e
Apple <apple@appurunoMacBook-Air.local>
2023-01-31 16:05:32 +0900

    コミット名

--- user_command.sh
+++ user_command.sh
@@ -2,6 +2,6 @@ test -f ~/shared/.env && ln -s ~/shared/.env ./.env || true
 export RAILS_ENV=production
 
 bundle install --deployment --without development,test --path vendor/bundle
-bin/rails db:create
+#bin/rails db:create
 bin/rails db:migrate
 bin/rails assets:precompile
