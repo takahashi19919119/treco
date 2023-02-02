7385fc92950ea889d954653265ed6033e68dcdbe
Apple <apple@appurunoMacBook-Air.local>
2023-02-02 18:53:12 +0900

    コミット名

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
 
 
-##
+##CSS反映の邪魔になるため、コメントアウト
   #config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
 
